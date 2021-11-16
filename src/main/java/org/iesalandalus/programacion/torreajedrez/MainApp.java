c5021d7234f57e1c37394b93a0c58a76fc5f8af0
Raul <Raul@LAPTOP-U3K61G9H>
2021-11-16 19:33:13 +0100

    kk

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -3,6 +3,6 @@ package org.iesalandalus.programacion.torreajedrez;
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("kkaa");
+		System.out.println("kk");
 	}
 }
