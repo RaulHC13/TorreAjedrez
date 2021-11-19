44df08edc77837f0c3d76cd81891ce6968a58d84
Raúl Herrerías Corral <raulhc1313@gmail.com>
2021-11-19 19:11:12 +0100

    Commit 3 (.6)

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -4,5 +4,24 @@ public class Posicion {
 	
 	private int fila;
 	private char columna;
+	
+	public int getFila() {
+		return fila;
+	}
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8)
+			throw new IllegalArgumentException("No esta comprendido entre 1 y 8");
+		this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	private void setColumna(char columna) {
+		if((columna > 'H' && columna <='Z')||(columna > 'h' && columna <='z')) {
+			throw new IllegalArgumentException("No esta comprendido entre A y H");
+		}
+		this.columna = columna;
+	}
+	
 
 }
