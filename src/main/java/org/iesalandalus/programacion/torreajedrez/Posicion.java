de98b780dd7d265d3ab9fcc3a1fe37006c44fb44
Raúl Herrerías Corral <raulhc1313@gmail.com>
2021-11-21 17:26:38 +0100

    Commit (.10)

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -5,14 +5,39 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	public Posicion(int fila, char columna){
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("No es un atributo correcto");	
+		}
+		setFila(fila);
+		
+		if((columna > 'H' && columna <='Z')||(columna > 'h' && columna <='z')) {
+			throw new IllegalArgumentException("No es un atributo correcto");
+		}
+		setColumna(columna);
+	}
+	
+	
+	public Posicion(Posicion posicion) {
+		
+		if (posicion == null) {
+			throw new NullPointerException("No se puede copiar una posición nula");
+		}
+			this.fila = posicion.getFila();
+			this.columna = posicion.getColumna();
+		
+	}
+	
 	public int getFila() {
 		return fila;
 	}
 	private void setFila(int fila) {
-		if (fila < 1 || fila > 8)
-			throw new IllegalArgumentException("No esta comprendido entre 1 y 8");
-		this.fila = fila;
-	}
+		if (fila < 1 || fila > 8) {
+		throw new IllegalArgumentException("No esta comprendido entre 1 y 8");	
+		}
+			this.fila = fila;
+			}
+	
 	public char getColumna() {
 		return columna;
 	}
@@ -22,6 +47,37 @@ public class Posicion {
 		}
 		this.columna = columna;
 	}
-	
 
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + columna;
+		result = prime * result + fila;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		if (columna != other.columna)
+			return false;
+		if (fila != other.fila)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Posicion: Fila = %s Columna = %s]", fila, columna);
+	}
+	
 }
